1b523635c6a06d8e1b44fb29ffb70a186354ed37
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-23 00:20:02 +0600

    add HTML and CSS in React

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,21 +6,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p>rs</p>
+      <Person></Person>
+      <Person></Person>
       </header>
     </div>
   );
+}
+
+function Person() {
+  return (
+    <div style= {{border:'2px solid red', margin:'10px'}}>
+      <h1>Name: Shonjoy</h1>
+      <h3>Hero of the year</h3>
+    </div>
+  )
 }
 
 export default App;
