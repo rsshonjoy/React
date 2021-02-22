cb29d14a547b513efca742e1fde8618d41ee5b34
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-23 01:26:41 +0600

    Pass dynamic data to components, props in react

--- src/App.js
+++ src/App.js
@@ -3,28 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const names = ['RS SHONJOY', 'RAJ']
   return (
     <div className="App">
       <header className="App-header">
       <p>rs</p>
-      <Person></Person>
-      <Person></Person>
+      <Person name={names[0]} food="Mango"></Person>
+      <Person name={names[1]} food="Apple"></Person>
       </header>
     </div>
   );
 }
 
-function Person() {
+function Person(props) {
   const personStyle = {
     border:'2px solid red',
     borderRadius:'5px',
     padding:'10px',
-    margin:'10px'
+    margin:'10px',
+    color: 'goldenrod'
   }
+  console.log(props);
+  
   return (
     <div style= {personStyle}>
-      <h1>Name: Shonjoy</h1>
-      <h3>Hero of the year</h3>
+      <h1>Name: {props.name}</h1>
+      <h3>My favorite fruit {props.food}.</h3>
     </div>
   )
 }
